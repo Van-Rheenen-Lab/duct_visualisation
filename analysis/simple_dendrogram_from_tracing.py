14f0485dcb9779b0718bb01e0a2dd16e9e070cac
Jeroen Doornbos <j.doornbos@nki.nl>
2025-10-06 16:11:55 +0200

    add demo json

--- analysis/simple_dendrogram_from_tracing.py
+++ analysis/simple_dendrogram_from_tracing.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 
 
 # json_path = r"I:\Group Rheenen\ExpDATA\2022_H.HRISTOVA\P004_TumorProgression_Myc\S005_Mouse_Puberty\E004_Imaging_carmine\06012025_carminegood images\export files\2516525-slide1_9weeks_branch.json"
-json_path = r'I:\Group Rheenen\ExpDATA\2022_H.HRISTOVA\P004_TumorProgression_Myc\S005_Mouse_Puberty\E004_Imaging_3D\2473536_Cft_24W\hierarchy tree.json'
+json_path = r'utils/demo_data/quick_example.json'
 
 graphs = load_duct_systems(json_path)
 g = create_directed_duct_graph(select_biggest_duct_system(graphs))
 g = simplify_graph(g)
-plot_hierarchical_graph(G=g,vert_gap=4, linewidth=0.4, font_size=10)
-plt.savefig("2473536_Cft_24W_uncolored.png", dpi=800)
+plot_hierarchical_graph(G=g,vert_gap=1, linewidth=2, font_size=30)
+# plt.savefig("Quick_example.png", dpi=800)
 plt.show()
